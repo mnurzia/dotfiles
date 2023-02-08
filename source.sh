f93c14a7ac1ebe7842a08735b08ee91f8d94d365
Max Nurzia <7797957+mnurzia@users.noreply.github.com>
2023-02-08 14:08:14 -0500

    fix source.sh

--- source.sh
+++ source.sh
@@ -5,12 +5,26 @@ _prompt() {
   PS1="$(~/.config/dotfiles/bin/prompt $?)"
 }
 
+# misc aliases
+alias ghostscript="/usr/bin/env gs"
+
+# ls aliases
 alias ls="ls --color=auto"
 alias l="ls"
 alias ll="ls -l"
 alias up="cd .."
+
+# git aliases
+alias gp="git push"
+alias ga="git add"
+alias gc="git commit"
+alias gs="git status"
+
+# custom binaries
+PATH="$PATH:~/.config/dotfiles/bin"
 alias colors=~/.config/dotfiles/bin/colors
 
+set EDITOR=nano
 HISTSIZE=-1
 HISTFILESIZE=-1
 INPUTRC=~/.config/dotfiles/inputrc
