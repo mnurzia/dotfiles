ea4ab9f90e12661135143c970dd2dc420884c8d2
Max Nurzia <7797957+mnurzia@users.noreply.github.com>
2023-03-06 18:02:38 -0500

    tweak colors

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -211,7 +211,7 @@ print_git_branch(int short_fmt)
     if (nl) {
       *nl = '\0';
     }
-    seg(0, 111);
+    seg(0, 253);
     if (!short_fmt) {
       if (git_rank > 1) {
         printf(" \ue725 [%i] %s ", git_rank, print_from);
