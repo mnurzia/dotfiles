ea03a1946d826aa2433cdd997304d5340337ccc0
Max Nurzia <7797957+mnurzia@users.noreply.github.com>
2023-03-06 17:59:37 -0500

    prompt changez

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -1,26 +1,54 @@
+#include <fcntl.h>
 #include <limits.h>
 #include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #define SEG_SEP "\xee\x82\xb0"
 
 int last_seg_bg = -1;
 
-void put_fg(int fg) { printf("\\[\x1b[38;5;%im\\]", fg); }
+int term_width = 80;
+int term_height = 24;
 
-void put_bg(int bg) { printf("\\[\x1b[48;5;%im\\]", bg); }
+void
+put_fg(int fg)
+{
+  printf("\\[\x1b[38;5;%im\\]", fg);
+}
 
-void put_sep(void) { printf(SEG_SEP); }
+void
+put_bg(int bg)
+{
+  printf("\\[\x1b[48;5;%im\\]", bg);
+}
 
-void clear(void) { printf("\\[\x1b[0m\\]"); }
+void
+put_sep(void)
+{
+  printf(SEG_SEP);
+}
 
-void seg_begin() { clear(); }
+void
+clear(void)
+{
+  printf("\\[\x1b[0m\\]");
+}
+
+void
+seg_begin()
+{
+  clear();
+}
 
 /* Output segment with foreground and background. */
-void seg(int fg, int bg) {
+void
+seg(int fg, int bg)
+{
   if (last_seg_bg != -1) {
     put_fg(last_seg_bg);
     put_bg(bg);
@@ -31,7 +59,9 @@ void seg(int fg, int bg) {
   last_seg_bg = bg;
 }
 
-void seg_end(void) {
+void
+seg_end(void)
+{
   clear();
   put_fg(last_seg_bg);
   put_sep();
@@ -40,18 +70,20 @@ void seg_end(void) {
   fflush(stdout);
 }
 
-void print_login_hostname() {
-  char *buf;
-  char *buf2;
+void
+print_login_hostname()
+{
+  char* buf;
+  char* buf2;
   struct passwd pwd;
-  struct passwd *out;
+  struct passwd* out;
   buf = malloc(80);
   buf2 = malloc(160);
   *buf = '\0';
   getpwuid_r(getuid(), &pwd, buf2, 160, &out);
   gethostname(buf, 80);
   {
-    char *c = buf;
+    char* c = buf;
     while (*c) {
       if (*c == '.') {
         *c = '\0';
@@ -64,25 +96,49 @@ void print_login_hostname() {
   free(buf2);
 }
 
-void print_wd() {
-  char *buf = malloc(PATH_MAX);
-  char *home = getenv("HOME");
-  char *wd = getcwd(buf, PATH_MAX);
+void
+print_wd(int short_fmt)
+{
+  char* buf = malloc(PATH_MAX);
+  char* home = getenv("HOME");
+  char* wd = getcwd(buf, PATH_MAX);
+  char* obuf = malloc(PATH_MAX);
+  *obuf = '\0';
   if (!wd) {
     wd = "<unlinked>";
   }
-  printf(" ");
   if (strncmp(home, wd, strlen(home)) == 0) {
-    printf("~");
+    strcat(obuf, "~");
     wd += strlen(home);
+    strcat(obuf, wd);
+  } else {
+    strcat(obuf, wd);
   }
-  printf("%s ", wd);
+  if (!short_fmt) {
+    printf(" %s ", obuf);
+  } else {
+    char* last_slash = strrchr(obuf, '/');
+    if (!last_slash) {
+      printf(" %s ", obuf);
+    } else if (last_slash == obuf && !*(last_slash + 1)) {
+      // "/"
+      printf(" %s ", obuf);
+    } else {
+      printf(" %s ", last_slash + 1);
+    }
+  }
 }
 
-void print_exit(int exit_code) { printf(" %i ", exit_code); }
+void
+print_exit(int exit_code)
+{
+  printf(" %i ", exit_code);
+}
 
-void seg_user_color() {
-  char *color = getenv("PROMPT_USER_COLORS");
+void
+seg_user_color()
+{
+  char* color = getenv("PROMPT_USER_COLORS");
   int fg, bg;
   if (getuid() == 0) {
     fg = 15;
@@ -97,7 +153,9 @@ void seg_user_color() {
   seg(fg, bg);
 }
 
-void print_hash() {
+void
+print_hash()
+{
   if (getuid() == 0) {
     printf(" # ");
   } else {
@@ -105,14 +163,93 @@ void print_hash() {
   }
 }
 
-int main(int argc, char **argv) {
+void
+print_git_branch(int short_fmt)
+{
+  char* wd_buf = malloc(PATH_MAX);
+  char* git_buf = malloc(PATH_MAX);
+  char* wd = getcwd(wd_buf, PATH_MAX);
+  char* branch_buf = malloc(PATH_MAX);
+  int git_rank = 0;
+  char head_buf[256] = "";
+  // suffix with / to normalize loop
+  strcat(wd_buf, "/");
+  if (!wd) {
+    return;
+  }
+  while (*wd) {
+    struct stat sb;
+    // copy path to temp
+    strcpy(git_buf, wd_buf);
+    // copy .git
+    strcpy(git_buf + (wd - wd_buf) + 1, ".git");
+    if (stat(git_buf, &sb) == 0 && S_ISDIR(sb.st_mode)) {
+      struct stat hb;
+      strcat(git_buf, "/HEAD");
+      if (stat(git_buf, &hb) == 0 && S_ISREG(hb.st_mode)) {
+        // read HEAD
+        int hfd = open(git_buf, O_RDONLY);
+        if (hfd != -1) {
+          read(hfd, head_buf, 256);
+          close(hfd);
+        }
+        git_rank++;
+      }
+    }
+    // advance to next dir
+    do {
+      wd++;
+    } while (*wd && *wd != '/');
+  }
+  if (head_buf[0]) {
+    char* last_slash = strrchr(head_buf, '/');
+    char* print_from = head_buf;
+    if (last_slash) {
+      print_from = last_slash + 1;
+    }
+    char* nl = strchr(head_buf, '\n');
+    if (nl) {
+      *nl = '\0';
+    }
+    seg(0, 111);
+    if (!short_fmt) {
+      if (git_rank > 1) {
+        printf(" \ue725 [%i] %s ", git_rank, print_from);
+      } else {
+        printf(" \ue725 %s ", print_from);
+      }
+    } else {
+      printf(" \ue725 ");
+    }
+  }
+}
+
+void
+get_dims()
+{
+  struct winsize sz;
+  if (ioctl(STDIN_FILENO, TIOCGWINSZ, &sz) == -1) {
+    return;
+  }
+  term_width = sz.ws_col;
+  term_height = sz.ws_row;
+}
+
+int
+main(int argc, char** argv)
+{
+  get_dims();
   int exit_code;
+  int short_fmt = term_width < 80;
   sscanf(argv[1], "%i", &exit_code);
   seg_begin();
-  seg(15, 236);
-  print_login_hostname();
+  if (!short_fmt) {
+    seg(15, 236);
+    print_login_hostname();
+  }
   seg(15, 234);
-  print_wd();
+  print_wd(short_fmt);
+  print_git_branch(short_fmt);
   if (exit_code != 0) {
     seg(15, 9);
     print_exit(exit_code);
