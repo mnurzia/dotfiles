e24eca9ae8cbf27a53cfdf484fa96fca1c43e5d1
Max Nurzia <7797957+mnurzia@users.noreply.github.com>
2023-06-05 11:13:37 -0400

    bviz: input files (untested)

--- bviz/bviz.c
+++ bviz/bviz.c
@@ -19,9 +19,25 @@ u32 make_color(u32 r, u32 g, u32 b, u32 a)
          ((a & 0xFF) << 24);
 }
 
-u32 pal_rgba(u32 i) { return i; }
+u32 lsb_ext(u32 in, u32 iw, u32 ow)
+{
+  return (in << (ow - iw)) | (((1 << (ow - iw)) - 1) * (in & 1));
+}
 
-u32 pal_xt256(u32 i)
+u32 pal_rgba8888(u32 i) /* db: 32 */ { return i; }
+
+u32 pal_rgb565(u32 i) /* db: 16 */
+{
+  int r = i & 0x1F, g = (i >> 5) & 0x3F, b = (i >> 11) & 0x1F;
+  return make_color(lsb_ext(r, 5, 8), lsb_ext(g, 6, 8), lsb_ext(b, 5, 8), 0xFF);
+}
+
+u32 pal_gray8(u32 i) /* db: 8 */
+{
+  return make_color(i & 0xFF, i & 0xFF, i & 0xFF, 0xFF);
+}
+
+u32 pal_xt8(u32 i) /* db: 8 */
 {
   i = i & 0xFF;
   if (i < 16) {
@@ -37,7 +53,30 @@ u32 pal_xt256(u32 i)
   }
 }
 
-u32 pal_bin(u32 i) { return 0xFFFFFFFF * !!i; }
+u32 pal_byteclass8(u32 i) /* db: 8 */
+{
+  if (!i)
+    return make_color(0x00, 0x00, 0x00, 0xFF);
+  else if (i < 0x20 && i != 0x09 && i != 0x0A && i != 0x0D)
+    return make_color(0x00, 0xFF, 0x00, 0xFF);
+  else if (i < 0x7F)
+    return make_color(0x00, 0x00, 0xFF, 0xFF);
+  else if (i < 0xFF)
+    return make_color(0xFF, 0x00, 0x00, 0xFF);
+  else
+    return make_color(0xFF, 0xFF, 0xFF, 0xFF);
+}
+
+u32 pal_bin(u32 i) /* db: 1-32 */
+{
+  return make_color(0xFF, 0xFF, 0xFF, 0xFF) * !!i;
+}
+
+void write_func(void* context, void* data, int size)
+{
+  FILE* f = (FILE*)context;
+  fwrite(data, 1, size, f);
+}
 
 int main(int argc, const char* const* argv)
 {
@@ -47,11 +86,13 @@ int main(int argc, const char* const* argv)
   int w = 16;
   size_t x = 0, y = 0;
   FILE* f = stdin;
+  FILE* fo = stdout;
   int bw = 8;
   u32 acc = 0;
   int acc_bits = 0;
   int leftover = 8;
-  const char* palette = "xt256";
+  const char* palette = "xt8";
+  const char* in_name = NULL;
   const char* out_name = "bviz.png";
   u32 (*pal_fn)(u32);
   aparse_state ap;
@@ -67,7 +108,13 @@ int main(int argc, const char* const* argv)
 
   aparse_add_opt(&ap, 'p', "palette");
   aparse_arg_type_str(&ap, &palette, NULL);
-  aparse_arg_help(&ap, "palette function, valid options are: xt256, rgba, bin");
+  aparse_arg_help(
+      &ap, "palette function, valid options are: xt8, rgba8888, rgb565, "
+           "gray8, bin, "
+           "byteclass8");
+
+  aparse_add_opt(&ap, 'i', NULL);
+  aparse_arg_type_str(&ap, &in_name, NULL);
 
   aparse_add_opt(&ap, 'o', NULL);
   aparse_arg_type_str(&ap, &out_name, NULL);
@@ -83,16 +130,25 @@ int main(int argc, const char* const* argv)
 
   assert(bw >= 1 && bw <= 32);
   assert(w > 0);
-  if (!strcmp(palette, "xt256")) {
-    pal_fn = &pal_xt256;
-  } else if (!strcmp(palette, "rgba")) {
-    pal_fn = &pal_rgba;
+  if (!strcmp(palette, "xt8")) {
+    pal_fn = &pal_xt8;
+  } else if (!strcmp(palette, "rgba8888")) {
+    pal_fn = &pal_rgba8888;
+  } else if (!strcmp(palette, "rgb565")) {
+    pal_fn = &pal_rgb565;
+  } else if (!strcmp(palette, "gray8")) {
+    pal_fn = &pal_gray8;
   } else if (!strcmp(palette, "bin")) {
     pal_fn = &pal_bin;
+  } else if (!strcmp(palette, "byteclass8")) {
+    pal_fn = &pal_byteclass8;
   } else {
     printf("invalid palette: %s\n", palette);
     return EXIT_FAILURE;
   }
+
+  if (in_name)
+    f = fopen(in_name, "r");
 
   while (!feof(f)) {
     size_t in = fread((void*)in_buf, 1, CHUNK_SZ, f);
@@ -126,8 +182,10 @@ int main(int argc, const char* const* argv)
       }
     }
   }
+  if (out_name)
+    fo = fopen(out_name, "w");
   if (y)
-    stbi_write_png(out_name, w, y + 1, 4, buf, 0);
+    stbi_write_png_to_func(&write_func, (void*)fo, w, y, 4, buf, 0);
   aparse_destroy(&ap);
   return 0;
 }
